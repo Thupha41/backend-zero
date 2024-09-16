e43cd10d6a9b62f41477462631a85fa3bca5b9b7
Thupha41 <phatngo.040103@gmail.com>
2024-09-16 23:11:03 +0700

    'changes'

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const { createServer } = require("node:http");
-const hostname = "127.0.0.1";
+const express = require("express");
+const app = express();
 const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
