8fa28584ea7c8bd30c3b4d253d2a09daa04a5415
Thupha41 <phatngo.040103@gmail.com>
2024-09-17 04:59:46 +0700

    Add static file

--- src/server.js
+++ src/server.js
@@ -9,6 +9,9 @@ const hostname = process.env.HOST_NAME;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//config static file
+app.use(express.static(path.join(__dirname, "public")));
+
 //Khai bao route
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
