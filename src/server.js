e570c4e549ff7b466796cf03d3a417f78ef40f47
Thupha41 <phatngo.040103@gmail.com>
2024-09-17 02:58:57 +0700

    Add nodemon package to automatically update when file js changes

--- src/server.js
+++ src/server.js
@@ -5,16 +5,14 @@ const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
-console.log(">>> check env: ", process.env);
-
 //config template engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //Khai bao route
 app.get("/", (req, res) => {
-  // res.send("Hello World!");
-  res.render("sample.ejs");
+  res.send("Hello World!!!");
+  // res.render("sample.ejs");
 });
 
 app.listen(port, hostname, () => {
