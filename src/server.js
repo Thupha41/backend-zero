b87f6a2e4743fcd5b1a433e9ae768f869ffae1bc
Thupha41 <phatngo.040103@gmail.com>
2024-09-18 16:41:23 +0700

    Create database connection

--- src/server.js
+++ src/server.js
@@ -5,9 +5,24 @@ const webRoutes = require("./routes/web");
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
-
+const mysql = require("mysql2");
 //config template engine
 configViewEngine(app);
+
+//test connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  port: 3307,
+  database: "thupha4141",
+  password: "123456",
+});
+
+// A simple SELECT query
+connection.query("SELECT * FROM Persons p", function (err, results, fields) {
+  console.log(">>> check results", results); // results contains rows returned by server
+  console.log(">>> check fields", fields); // fields contains extra meta data about results, if available
+});
 
 //Khai bao route
 app.use("/", webRoutes);
