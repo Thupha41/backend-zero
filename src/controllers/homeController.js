5998db74074a117fa7b7cdbdd60220a3e43f402f
Thupha41 <phatngo.040103@gmail.com>
2024-09-19 21:59:28 +0700

    Create home page file and Add navbar desgin

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,16 +1,7 @@
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-  //process data
-  //call model
-  let users = [];
-  // A simple SELECT query
-  connection.query("SELECT * FROM Persons p", function (err, results, fields) {
-    users = results;
-    console.log(">>> check results", results); // results contains rows returned by server
-    // res.render("sample.ejs");
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home.ejs");
 };
 
 module.exports = {
