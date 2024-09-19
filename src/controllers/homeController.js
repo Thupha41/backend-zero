e95ffbfee63fce2f001a93aa3b2976b4200b1b8c
Thupha41 <phatngo.040103@gmail.com>
2024-09-20 02:26:27 +0700

    Create connection query to store data from user input into db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,8 +4,15 @@ const getHomePage = (req, res) => {
   return res.render("home.ejs");
 };
 const postCreateUser = (req, res) => {
+  let { email, firstName, lastName, city, address } = req.body; //Destructuring
+  connection.query(
+    `INSERT INTO Persons (LastName, FirstName, Address, City, Email) VALUES (?, ?, ?, ?, ?)`,
+    [lastName, firstName, address, city, email],
+    function (err, results) {
+      res.send("Create user success!!!");
+    }
+  );
   console.log(">>> req.body", req.body);
-  res.send("Create new user");
 };
 module.exports = {
   getHomePage,
