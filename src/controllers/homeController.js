b27361fa3a684578ee965ada05ba1727f74bcc34
Thupha41 <phatngo.040103@gmail.com>
2024-09-23 01:15:56 +0700

    Create getCreateUserPage and optimize connection query with async await

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,18 +3,21 @@ const connection = require("../config/database");
 const getHomePage = (req, res) => {
   return res.render("home.ejs");
 };
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
   let { email, firstName, lastName, city, address } = req.body; //Destructuring
-  connection.query(
+  let [rows, fields] = await connection.query(
     `INSERT INTO Persons (LastName, FirstName, Address, City, Email) VALUES (?, ?, ?, ?, ?)`,
-    [lastName, firstName, address, city, email],
-    function (err, results) {
-      res.send("Create user success!!!");
-    }
+    [lastName, firstName, address, city, email]
   );
-  console.log(">>> req.body", req.body);
+  console.log("Check result", rows);
+  res.send("Create user success!!!");
+};
+
+const getCreatePage = (req, res) => {
+  return res.render("register.ejs");
 };
 module.exports = {
   getHomePage,
   postCreateUser,
+  getCreatePage,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -2,12 +2,14 @@ const express = require("express");
 const {
   getHomePage,
   postCreateUser,
+  getCreatePage,
 } = require("../controllers/homeController");
 const router = express.Router();
 
 router.get("/abc", (req, res) => {
   res.send("check Abc");
 });
+router.get("/create", getCreatePage);
 
 router.get("/", getHomePage);
 router.post("/create-user", postCreateUser);
